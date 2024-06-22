e86b51cde3fb8516152b03481b3c770bf6637730
Peal Hasan <pealh0320@gmail.com>
2024-06-21 20:12:59 -0400

    Npm packages installed

---
+++ app.js
@@ -0,0 +1,33 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import pg from "pg";
+import bcrypt from "bcrypt";
+import passport from "passport";
+import { Strategy } from "passport-local";
+import GoogleStrategy from "passport-google-oauth2";
+import session from "express-session";
+import env from "dotenv";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = 3000;
+
+// This line let us use .body method
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Get request
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+// Post data
+app.post("/submit", (req, res) => {
+  res.send(`<h1>Your band name is:</h1><h2>${bandName}✌️</h2>`);
+});
+
+// Listening port
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
