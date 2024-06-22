5f752dd8335ca6793e2c5a8bd312da48c0ce2639
Peal Hasan <pealh0320@gmail.com>
2024-06-22 17:52:02 -0400

    PostgreSQL database created and established connection in Node.js, Session Cookies added

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import env from "dotenv";
 import sgMail from "@sendgrid/mail";
+import pg from "pg";
+import bcrypt from "bcrypt";
+import passport from "passport";
+import { Strategy } from "passport-local";
+import GoogleStrategy from "passport-google-oauth2";
+import session from "express-session";
 
 // Load environment variables from .env file
 env.config();
@@ -11,16 +17,44 @@ env.config();
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
-
+const saltRounds = 10;
 // Set SendGrid API key
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 // This line let us use .body method
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());  // To parse JSON bodies
+app.use(bodyParser.json()); // To parse JSON bodies
 
 // Declare static file public
 app.use(express.static("public"));
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
+
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
+  })
+);
+
+app.use(passport.initialize());
+
+app.use(passport.session());
+
+// Database connection
+const db = new pg.Client({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+});
+
+db.connect();
 
 // Generate a random OTP
 const generateRandomOTP = () => {
@@ -34,23 +68,55 @@ app.post("/send-otp", (req, res) => {
 
   const msg = {
     to: email,
-    from: 'mrdeadstark@gmail.com', // Change to your verified sender
-    subject: 'Your OTP Code',
+    from: "mrdeadstark@gmail.com", // Change to your verified sender
+    subject: "Your CricScore OTP Code is here",
     text: `Your OTP code is ${otp}`,
     html: `<strong>Your OTP code is ${otp}</strong>`,
   };
 
-  sgMail.send(msg)
-    .then(() => {
-      console.log('Email sent');
+  sgMail
+    .send(msg)
+    .then(async () => {
+      console.log("Email sent");
       res.status(200).send({ success: true, otp });
     })
     .catch((error) => {
-      console.error('Error sending email:', error); // Log the error details
+      console.error("Error sending email:", error); // Log the error details
       res.status(500).send({ success: false, error: error.message });
     });
 });
 
+app.post("/register", async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  try {
+    const checkResult = await db.query("SELECT * FROM auth WHERE email = $1", [
+      email,
+    ]);
+    if (checkResult.rows.length > 0) {
+      res.redirect("/login");
+    } else {
+      bcrypt.hash(password, saltRounds, async (err, hash) => {
+        if (err) {
+          console.log("Error hashing password");
+        } else {
+          const result = await db.query(
+            "INSERT INTO auth (email, password) VALUES ($1, $2) RETURNING *",
+            [email, hash]
+          );
+          const user = result.rows[0];
+            req.login(user, (err) => {
+            console.log("Success");
+            res.redirect("/dashboard");
+          });
+        }
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
 // Get requests
 app.get("/", (req, res) => {
   res.render("main.ejs");
@@ -60,12 +126,51 @@ app.get("/signin", (req, res) => {
   res.render("login.ejs");
 });
 
+app.post("/signin", passport.authenticate("local", {
+  successRedirect: "/dashboard",
+  failureRedirect: "/login",
+}))
+
 app.get("/signup", (req, res) => {
   res.render("signup.ejs");
 });
 
 app.get("/dashboard", (req, res) => {
-  res.render("<h1> Success </h1>");
+  res.render("dashboard.ejs");
+});
+
+passport.use("local", new Strategy(async function verify(username, password, cb) {
+  try{
+    const result = await db.query("SELECT * FROM auth WHERE email = $1", [username]);
+    if(result.rows.length > 0){
+      const user = result.rows[0];
+      const hashedPass = user.password;
+      bcrypt.compare(password, hashedPass, (err, valid) => {
+        if(err) {
+          console.log("Error comparing password");
+          return cb(err);
+        } else {
+          if(valid){
+            return cb(null, user);
+          } else {
+            return cb(null, false);
+          }
+        }
+      });
+    } else {
+      return cb("User not found");
+    }
+  } catch(err) {
+    console.log(err);
+  }
+}))
+
+passport.serializeUser((user, cb) => {
+  cb(null, user);
+});
+
+passport.deserializeUser((user, cb) => {
+  cb(null, user);
 });
 
 // Listening port
